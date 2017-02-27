d696a062acb40050fae47d9a4f12179b1421a9c6
Marc Ribalta Gene <mariva1995@gmail.com>
2017-02-27 22:35:39 +0100

    v2

--- PacktWebScraping.py
+++ PacktWebScraping.py
@@ -7,15 +7,19 @@ Client web simple per obtenir nom de llibre de PACKT
 @author: mrg20@alumnes.udl.cat
 '''
 
-
+import urllib2
 
 class Client():
+	page = "https://www.packtpub.com/packt/offers/free-learning/"
 
 	def main(self):
 		#obtenir web
+		f = urllib2.urlopen(Client.page)
+		htmlpage = f.read()
+		f.close()
+		print htmlpage
 		#buscar dades
 		#imprimir
-		pass
 
 
 
