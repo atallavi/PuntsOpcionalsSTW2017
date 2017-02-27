4e85e9d7277cf6087dc394c7aa47339f94e7d268
Marc Ribalta Gene <mariva1995@gmail.com>
2017-02-27 22:30:05 +0100

    v1

---
+++ PacktWebScraping.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8 :
+'''
+Client web simple per obtenir nom de llibre de PACKT
+
+@author: mrg20@alumnes.udl.cat
+'''
+
+
+
+class Client():
+
+	def main(self):
+		#obtenir web
+		#buscar dades
+		#imprimir
+		pass
+
+
+
+
+
+if __name__ == '__main__':
+	wc = Client()
+	wc.main()
